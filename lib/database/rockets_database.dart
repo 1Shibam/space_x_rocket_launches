ac0602c6f3233dae97a96c6d0d14484547b4b9e6
1Shibam <hehe.shibam@gmail.com>
2025-03-03 14:59:11 +0530

    update rockets database

--- lib/database/rockets_database.dart
+++ lib/database/rockets_database.dart
@@ -1,4 +1,54 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-final rocketDataBaseProvider = FutureProvider<Database>((ref) {});
+final rocketDataBaseProvider = FutureProvider<Database>((ref) async {
+  final dbPath = await getApplicationDocumentsDirectory();
+  final pathDirectory = join(dbPath.path, 'rocketsDB.db');
+
+  return openDatabase(
+    pathDirectory,
+    version: 1,
+    onCreate: (rocketsDatabase, version) {
+      rocketsDatabase.execute('''
+    CREATE TABLE rocketsData(
+    rocketID INTEGER PRIMARY KEY AUTOINCREMENT,
+    rocketName TEXT,
+    activeStatus INTEGER DEFAULT 0,
+    costPerLaunch INTEGER,
+    successRate INTEGER,
+    country TEXT,
+    wikipediaSource TEXT,
+    description TEXT,
+    heightInMeters REAL,
+    heightInFeets REAL,
+    diameterInMeter REAL,
+    diameterInFeets REAL,
+    massInKg REAL,
+    massInLbs REAL
+
+    )
+''');
+    },
+  );
+});
+
+/*
+rocketID: map['rocketID'] as int?,
+        rocketName: map[''],
+        activeStatus: map[''],
+        costPerLaunch: map[''],
+        successRate: map[''],
+        firstLaunch: map[''],
+        country: map[''],
+        wikipediaSource: map[''],
+        description: map[''],
+
+        heightInMeters: map[''],
+        heightInFeets: map[''],
+        diameterInMeter: map[''],
+        diameterInFeets: map[''],
+        massInKg: map[''],
+        massInLbs: map['']);
+ */
