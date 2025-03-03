12d1e993eacca088d907979b51561efc48063442
1Shibam <hehe.shibam@gmail.com>
2025-03-03 06:01:04 +0530

    launches list provider done

---
+++ lib/providers/launches_list_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:space_x_rocket_launches/models/launches_data_model.dart';
+import 'package:space_x_rocket_launches/services/space_x_services.dart';
+
+class LaunchesListNotifer
+    extends StateNotifier<AsyncValue<List<LaunchesDataModel>>> {
+  LaunchesListNotifer(this.services) : super(const AsyncValue.loading()) {
+    fetchLaunchesList();
+  }
+  SpaceXServices services;
+
+  Future<void> fetchLaunchesList() async {
+    try {
+      state = const AsyncValue.loading();
+      final launches = await services.getLaunchesList();
+      state = AsyncValue.data(launches);
+    } catch (error, stackTrace) {
+      state = AsyncValue.error(error, stackTrace);
+    }
+  }
+}
+
+final launchesListNotifierStateProvider = StateNotifierProvider<
+        LaunchesListNotifer, AsyncValue<List<LaunchesDataModel>>>(
+    (ref) => LaunchesListNotifer(SpaceXServices()));
