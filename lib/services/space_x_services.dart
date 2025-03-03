e8fa09a54def776ac2fa7e6c1a31b75763d2de79
1Shibam <hehe.shibam@gmail.com>
2025-03-03 06:05:12 +0530

    adds get roket list method

--- lib/services/space_x_services.dart
+++ lib/services/space_x_services.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:space_x_rocket_launches/models/launches_data_model.dart';
-
+import 'package:space_x_rocket_launches/models/rockets_data_model.dart';
 
 class SpaceXServices {
   final Dio dio = Dio(BaseOptions(
@@ -16,9 +16,26 @@ class SpaceXServices {
       if (respone.statusCode == 200) {
         final List<Map<String, dynamic>> data =
             List<Map<String, dynamic>>.from(respone.data);
-        print(data);
+
         final result =
             data.map((json) => LaunchesDataModel.fromJson(json)).toList();
+        return result;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
+
+  Future<List<RocketsDataModel>> getRocketsList() async {
+    try {
+      final response = await dio.get('/rockets');
+      if (response.statusCode == 200) {
+        final List<Map<String, dynamic>> data =
+            List<Map<String, dynamic>>.from(response.data);
+        final result =
+            data.map((json) => RocketsDataModel.fromJson(json)).toList();
         return result;
       } else {
         return [];
