779263bffa1bced529e8acdbaba34b46de64e7e2
1Shibam <hehe.shibam@gmail.com>
2025-03-03 15:13:40 +0530

    add delete and fetch list methods

--- lib/services/launches_database_service.dart
+++ lib/services/launches_database_service.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:space_x_rocket_launches/database/launches_database.dart';
 import 'package:space_x_rocket_launches/models/launches_data_model.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -6,6 +8,7 @@ class LaunchesDatabaseService {
   final Database database;
   LaunchesDatabaseService(this.database);
 
+  //? adds the launches data to db
   Future<void> addLaunchesToDB(LaunchesDataModel launch) async {
     try {
       await database.insert('launchesTable', launch.toJson(),
@@ -15,4 +18,23 @@ class LaunchesDatabaseService {
       rethrow;
     }
   }
+
+  //? Fetches the list of launches saved in the database
+  Future<List<LaunchesDataModel>> getListOfLaunchesFromDB() async {
+    final List<Map<String, dynamic>> launches =
+        await database.query('launchesTable');
+    return launches.map((data) => LaunchesDataModel.fromDBMap(data)).toList();
+  }
+
+  //? delete the saved launches from the database
+  Future<int> deleteLaunchFromDB(int launchID) async {
+    return await database
+        .delete('launchesTable', where: 'launchID = ?', whereArgs: [launchID]);
+  }
 }
+
+final launchesDatabaseServiceProvider =
+    FutureProvider<LaunchesDatabaseService>((ref) async {
+  final launchesDB = await ref.watch(launchesDatabaseProvider.future);
+  return LaunchesDatabaseService(launchesDB);
+});
