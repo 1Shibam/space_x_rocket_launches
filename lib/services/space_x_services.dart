b203326fbd0a4b1ab6cdbafd54c556c766ec5435
1Shibam <hehe.shibam@gmail.com>
2025-03-03 06:00:56 +0530

    launches list api call done

--- lib/services/space_x_services.dart
+++ lib/services/space_x_services.dart
@@ -1,10 +1,30 @@
 import 'package:dio/dio.dart';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:space_x_rocket_launches/models/launches_data_model.dart';
+
 
 class SpaceXServices {
   final Dio dio = Dio(BaseOptions(
-    baseUrl: dotenv.env['API_END_POINT']!,
-    receiveTimeout: const Duration(seconds: 5),
-    connectTimeout: const Duration(seconds: 5)
-  ));
+      baseUrl: dotenv.env['API_END_POINT']!,
+      receiveTimeout: const Duration(seconds: 5),
+      connectTimeout: const Duration(seconds: 5)));
+
+  Future<List<LaunchesDataModel>> getLaunchesList() async {
+    try {
+      final respone = await dio.get('/launches');
+      if (respone.statusCode == 200) {
+        final List<Map<String, dynamic>> data =
+            List<Map<String, dynamic>>.from(respone.data);
+        print(data);
+        final result =
+            data.map((json) => LaunchesDataModel.fromJson(json)).toList();
+        return result;
+      } else {
+        return [];
+      }
+    } catch (e) {
+      return [];
+    }
+  }
 }
