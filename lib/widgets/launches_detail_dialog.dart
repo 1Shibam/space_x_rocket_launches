700ae540e8b771c61bfdcac9bc5255e131aac4c8
1Shibam <hehe.shibam@gmail.com>
2025-03-03 16:08:04 +0530

    add detail widget for the tile on tap feature

--- lib/widgets/launches_detail_dialog.dart
+++ lib/widgets/launches_detail_dialog.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
-class LaunchesDetailScreen extends StatelessWidget {
-  const LaunchesDetailScreen(
+class LaunchesDetailDialog extends StatelessWidget {
+  const LaunchesDetailDialog(
       {super.key,
       required this.flightNumber,
+      required this.rocketName,
       required this.videoSource,
       required this.articleSource,
       required this.wikipediaSource,
@@ -12,6 +13,7 @@ class LaunchesDetailScreen extends StatelessWidget {
       required this.landinDate,
       required this.successStatus});
   final int flightNumber;
+  final String rocketName;
   final String videoSource;
   final String articleSource;
   final String wikipediaSource;
@@ -22,21 +24,6 @@ class LaunchesDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const AlertDialog();
   }
 }
-
-/*
- final int? launchID;
-  final int flightNumber;
-  final String videoSource;
-  final String articleSoruce;
-  final String wikipediaSource;
-  final String missionName;
-  final String rocket;
-  final String landingDate;
-  final bool successStatus;
- */
-
-//5e9d0d95eda69955f709d1eb
-//5e9d0d95eda69955f709d1eb
--- lib/widgets/rockets_detail_screen.dart
+++ lib/widgets/rockets_detail_screen.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
 
 class RocketsDetailScreen extends StatelessWidget {
-  const RocketsDetailScreen({super.key});
+  const RocketsDetailScreen(
+      {super.key,
+      required this.rocketName,
+      required this.activeStatus,
+      required this.costPerLaunch,
+      required this.successRate,
+      required this.firstLauch,
+      required this.country,
+      required this.wikipediaSource,
+      required this.description,
+      required this.heightInMeters,
+      required this.heightInFeet,
+      required this.diameterInMeter,
+      required this.diameterInFeet,
+      required this.massInKg,
+      required this.massInLbs});
+  final String rocketName;
+  final bool activeStatus;
+  final int costPerLaunch;
+  final int successRate;
+  final String firstLauch;
+  final String country;
+  final String wikipediaSource;
+  final String description;
+  final double heightInMeters;
+  final double heightInFeet;
+  final double diameterInMeter;
+  final double diameterInFeet;
+  final double massInKg;
+  final double massInLbs;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold();
   }
 }
+
+/*
+  final int? rocketID;
+  final String rocketName;
+  final bool activeStatus;
+  final int costPerLaunch;
+  final int successRate;
+  final String firstLaunch;
+  final String country;
+  final String wikipediaSource;
+  final String description;
+  final String rocket;
+
+  final double heightInMeters;
+  final double heightInFeets;
+  final double diameterInMeter;
+  final double diameterInFeets;
+  final double massInKg;
+  final double massInLbs;
+ */
